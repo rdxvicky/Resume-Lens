681c8d1f5cb380a28276909154afa457a3b7c0ec
rdx_vicky <rdxvicky33@gmail.com>
2024-05-14 23:22:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ input_prompt1 = """
 """
 
 input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science,Frontend, Backend, Cloud etc. engineering jobs and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
 """
